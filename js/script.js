a5508323f328e16548bf4a04006947aac304b3f6
PrestonC <1123preston1123@gmail.com>
2022-09-30 22:39:42 -0500

    updated home style/initial creation of other pages

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    setupLinks();
+})
+
+
+function setupLinks() {
+    $("#deals").width($("#locations").width());
+    $(".buttonLinks").removeClass("hidden");
+
+    $("#products").on("click", function() {
+        toggleDecks(["locationsDeck", "strainsDeck"], ["none", "grid"]);
+    });
+
+    $("#locations").on("click", function() {
+        toggleDecks(["locationsDeck", "strainsDeck"], ["grid", "none"]);
+    });
+
+    $("#deals").on("click", function() {
+        toggleDecks(["locationsDeck", "strainsDeck"], ["none", "none"]);
+    });
+
+    $("#news").on("click", function() {
+        toggleDecks(["locationsDeck", "strainsDeck"], ["none", "none"]);
+    });
+}
+
+function toggleDecks(deckList, newDisplay) {
+    for (let index = 0; index < deckList.length; index++) {
+        const element = deckList[index];
+        $("."+element).css("display", newDisplay[index]);
+    }
+}
